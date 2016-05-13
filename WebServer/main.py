3a9cd4836999b3aebab96eb399ce41e8f28bff5e
ComputerAided <ComputerAided@users.noreply.github.com>
2016-05-13 07:22:44 -0600

    Update main.py
    Added datetime support

--- WebServer/main.py
+++ WebServer/main.py
@@ -1,10 +1,23 @@
-import datetime
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("main.html")
+    now = datetime.now()
+    timeString = strftime("%Y-%m-%d %H:%M")
+    
+    date = int(timeString[8:10])
+    
+    dl = 20 - date
+    DL = str(dl)
+    
+    templateData = {
+        'title' : "Hello",
+        'time' : timeString,
+        'DL' : DL
+    }
+    return render_template("main.html", **templateData)
 
 if __name__ == "__main__":
     app.run()
